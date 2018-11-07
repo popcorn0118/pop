4604599cbe69a796831a535677e86dd51b8e3bda
popcorn <popcorn0118@gmail.com>
2018-11-07 22:42:59 +0800

    選購產品及加入購物車

--- project/src/components/pages/CustomerOrders.vue
+++ project/src/components/pages/CustomerOrders.vue
@@ -27,7 +27,8 @@
                             <i class="fas fa-spinner fa-spin"  v-if="item.id === status.loadingItem"></i>
                             查看更多
                         </button>
-                        <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
+                        <button type="button" class="btn btn-outline-danger btn-sm ml-auto"
+                            @click="addtoCart(item.id)">
                             <i class="fas fa-spinner fa-spin"  v-if="item.id === status.loadingItem"></i>
                             加到購物車
                         </button>
@@ -48,13 +49,13 @@
                     <div class="modal-body">
                         <img :src="product.imageUrl" class="img-fluid" alt="">
                         <blockquote class="blockquote mt-3">
-                        <p class="mb-0">{{ product.content }}</p>
-                        <footer class="blockquote-footer text-right">{{ product.description }}</footer>
+                            <p class="mb-0">{{ product.content }}</p>
+                            <footer class="blockquote-footer text-right">{{ product.description }}</footer>
                         </blockquote>
                         <div class="d-flex justify-content-between align-items-baseline">
-                        <div class="h4" v-if="!product.price">{{ product.origin_price }} 元</div>
-                        <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
-                        <div class="h4" v-if="product.price">現在只要 {{ product.price }} 元</div>
+                            <div class="h4" v-if="!product.price">{{ product.origin_price }} 元</div>
+                            <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
+                            <div class="h4" v-if="product.price">現在只要 {{ product.price }} 元</div>
                         </div>
                         <select name="" class="form-control mt-3" v-model="product.num">
                         <option :value="num" v-for="num in 10" :key="num">
@@ -84,39 +85,64 @@ import $ from 'jquery';
 export default {
   data() {
     return {
-      products: [],
-      product: {},
-      status: {
-          loadingItem: ''
-      },
-      isLoading: false,
+        products: [],
+        product: {},
+        status: {
+            loadingItem: ''
+        },
+        isLoading: false,
     };
   },
   methods: {
     getProducts() {
-      const vm = this;
-      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
-      vm.isLoading = true;
-      this.$http.get(url).then((response) => {
-        vm.products = response.data.products;
-        console.log(response);
-        vm.isLoading = false;
-      });
+        const vm = this;
+        const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+        vm.isLoading = true;
+        this.$http.get(url).then((response) => {
+            vm.products = response.data.products;
+            console.log(response);
+            vm.isLoading = false;
+        });
     },
     getProduct(id) {
+        const vm = this;
+        const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+        vm.status.loadingItem = id;
+        this.$http.get(url).then((response) => {
+            vm.product = response.data.product;
+            $('#productModal').modal('show');
+            console.log(response);
+            vm.status.loadingItem = '';
+        });
+    },
+    addtoCart(id, qty = 1) {
       const vm = this;
-      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
       vm.status.loadingItem = id;
-      this.$http.get(url).then((response) => {
-        vm.product = response.data.product;
-        $('#productModal').modal('show');
+      const cart = {
+        product_id: id,
+        qty,
+      };
+      this.$http.post(url, { data: cart }).then((response) => {
         console.log(response);
         vm.status.loadingItem = '';
+        vm.this.getCart();
+        $('#productModal').modal('hide');
       });
     },
+    getCart() {
+        const vm = this;
+        const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+        vm.isLoading = true;
+        this.$http.get(url).then((response) => {
+            console.log(response);
+            vm.isLoading = false;
+        });
+    },
   },
-  created() {
+created() {
     this.getProducts();
+    this.getCart();
   },
 };
 </script>
